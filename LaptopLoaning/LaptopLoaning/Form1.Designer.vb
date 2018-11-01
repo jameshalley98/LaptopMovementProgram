137ac5178040e0c9c53ed1470a3a53f385a9e521
jameshalley98 <jameshalley98@gmail.com>
2018-11-01 15:16:14 +1100

    Add files via upload

---
+++ LaptopLoaning/LaptopLoaning/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtSerialNumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbLocation1 = New System.Windows.Forms.ComboBox()
+        Me.cmbLocation2 = New System.Windows.Forms.ComboBox()
+        Me.cmbLocation3 = New System.Windows.Forms.ComboBox()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.txtFileLocation = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtSerialNumber
+        '
+        Me.txtSerialNumber.Location = New System.Drawing.Point(108, 6)
+        Me.txtSerialNumber.Name = "txtSerialNumber"
+        Me.txtSerialNumber.Size = New System.Drawing.Size(202, 20)
+        Me.txtSerialNumber.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Machine Name(s)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Location"
+        '
+        'cmbLocation1
+        '
+        Me.cmbLocation1.FormattingEnabled = True
+        Me.cmbLocation1.Location = New System.Drawing.Point(108, 32)
+        Me.cmbLocation1.Name = "cmbLocation1"
+        Me.cmbLocation1.Size = New System.Drawing.Size(202, 21)
+        Me.cmbLocation1.TabIndex = 4
+        '
+        'cmbLocation2
+        '
+        Me.cmbLocation2.FormattingEnabled = True
+        Me.cmbLocation2.Location = New System.Drawing.Point(108, 60)
+        Me.cmbLocation2.Name = "cmbLocation2"
+        Me.cmbLocation2.Size = New System.Drawing.Size(202, 21)
+        Me.cmbLocation2.TabIndex = 5
+        Me.cmbLocation2.Visible = False
+        '
+        'cmbLocation3
+        '
+        Me.cmbLocation3.FormattingEnabled = True
+        Me.cmbLocation3.Location = New System.Drawing.Point(108, 87)
+        Me.cmbLocation3.Name = "cmbLocation3"
+        Me.cmbLocation3.Size = New System.Drawing.Size(202, 21)
+        Me.cmbLocation3.TabIndex = 7
+        Me.cmbLocation3.Visible = False
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Enabled = False
+        Me.btnConfirm.Location = New System.Drawing.Point(235, 114)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(75, 20)
+        Me.btnConfirm.TabIndex = 9
+        Me.btnConfirm.Text = "Confirm"
+        Me.btnConfirm.UseVisualStyleBackColor = True
+        '
+        'txtFileLocation
+        '
+        Me.txtFileLocation.Location = New System.Drawing.Point(108, 114)
+        Me.txtFileLocation.Name = "txtFileLocation"
+        Me.txtFileLocation.Size = New System.Drawing.Size(120, 20)
+        Me.txtFileLocation.TabIndex = 10
+        Me.txtFileLocation.Text = "C:\Where Laptop Go\WhereWent.txt"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "File to use"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 142)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtFileLocation)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.cmbLocation3)
+        Me.Controls.Add(Me.cmbLocation2)
+        Me.Controls.Add(Me.cmbLocation1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSerialNumber)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Where laptop go?"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSerialNumber As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbLocation1 As ComboBox
+    Friend WithEvents cmbLocation2 As ComboBox
+    Friend WithEvents cmbLocation3 As ComboBox
+    Friend WithEvents btnConfirm As Button
+    Friend WithEvents txtFileLocation As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ LaptopLoaning/LaptopLoaning/Form1.vb
@@ -0,0 +1,154 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim filePath As String = "C:\Where Laptop Go\"
+
+    Dim locations1 = File.ReadAllLines(filePath + "Locations1.txt")
+    Dim ABlockLevels = File.ReadAllLines(filePath + "ABlockLevels.txt")
+    Dim ABlockRoomsL0 = File.ReadAllLines(filePath + "ABlockRoomsL0.txt")
+    Dim ABlockRoomsL1 = File.ReadAllLines(filePath + "ABlockRoomsL1.txt")
+    Dim BBlockLevels = File.ReadAllLines(filePath + "BBlockLevels.txt")
+    Dim BBlockRoomsL0 = File.ReadAllLines(filePath + "BBlockRoomsL0.txt")
+    Dim BBlockRoomsL1 = File.ReadAllLines(filePath + "BBlockRoomsL1.txt")
+    Dim CBlockLevels = File.ReadAllLines(filePath + "CBlockLevels.txt")
+    Dim CBlockRoomsL0 = File.ReadAllLines(filePath + "CBlockRoomsL0.txt")
+    Dim CBlockRoomsL1 = File.ReadAllLines(filePath + "CBlockRoomsL1.txt")
+    Dim CBlockRoomsL2 = File.ReadAllLines(filePath + "CBlockRoomsL2.txt")
+    Dim EBlockRooms = File.ReadAllLines(filePath + "EBlockRooms.txt")
+    Dim Demountables = File.ReadAllLines(filePath + "Demountables.txt")
+    Dim Others = File.ReadAllLines(filePath + "Others.txt")
+
+    Dim SpecialCharacter As Char() = {"\"c}
+    Dim Separator As String = "----"
+
+    Dim WhereWent As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        cmbLocation1.Items.AddRange(locations1)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbLocation1.SelectedIndexChanged
+
+        cmbLocation2.Visible = True
+        cmbLocation2.Items.Clear()
+        cmbLocation2.Text = ""
+        cmbLocation3.Visible = False
+        cmbLocation3.Items.Clear()
+        btnConfirm.Enabled = False
+
+        Select Case cmbLocation1.Text
+            Case locations1(0)
+                cmbLocation2.Items.AddRange(ABlockLevels)
+                cmbLocation3.Visible = True
+            Case locations1(1)
+                cmbLocation2.Items.AddRange(BBlockLevels)
+                cmbLocation3.Visible = True
+            Case locations1(2)
+                cmbLocation2.Items.AddRange(CBlockLevels)
+                cmbLocation3.Visible = True
+            Case locations1(3)
+                cmbLocation2.Items.AddRange(EBlockRooms)
+            Case locations1(4)
+                cmbLocation2.Items.AddRange(Demountables)
+            Case locations1(5)
+                cmbLocation2.Items.AddRange(Others)
+
+        End Select
+
+    End Sub
+
+    Private Sub cmbLocation2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbLocation2.SelectedIndexChanged
+
+        btnConfirm.Enabled = False
+        cmbLocation3.Items.Clear()
+        cmbLocation3.Text = ""
+
+        Select Case cmbLocation2.Text
+            Case ABlockLevels(0)
+                cmbLocation3.Items.AddRange(ABlockRoomsL0)
+                cmbLocation3.Visible = True
+            Case ABlockLevels(1)
+                cmbLocation3.Items.AddRange(ABlockRoomsL1)
+                cmbLocation3.Visible = True
+            Case BBlockLevels(0)
+                cmbLocation3.Items.AddRange(BBlockRoomsL0)
+                cmbLocation3.Visible = True
+            Case BBlockLevels(1)
+                cmbLocation3.Items.AddRange(BBlockRoomsL1)
+                cmbLocation3.Visible = True
+            Case CBlockLevels(0)
+                cmbLocation3.Items.AddRange(CBlockRoomsL0)
+                cmbLocation3.Visible = True
+            Case CBlockLevels(1)
+                cmbLocation3.Items.AddRange(CBlockRoomsL1)
+                cmbLocation3.Visible = True
+            Case CBlockLevels(2)
+                cmbLocation3.Items.AddRange(CBlockRoomsL2)
+                cmbLocation3.Visible = True
+            Case Else
+                btnConfirm.Enabled = True
+
+        End Select
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtSerialNumber.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            txtSerialNumber.Text += "\"
+            txtSerialNumber.SelectionStart = Len(txtSerialNumber.Text)
+        End If
+    End Sub
+
+    Private Sub cmbLocation3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbLocation3.SelectedIndexChanged
+        btnConfirm.Enabled = True
+    End Sub
+
+    Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        Dim WhereWentLines = File.ReadAllLines(WhereWent)
+
+        If txtSerialNumber.Text(Len(txtSerialNumber.Text) - 1) = SpecialCharacter Then
+            txtSerialNumber.Text = txtSerialNumber.Text.Remove(txtSerialNumber.Text.Length - 1)
+        End If
+        Dim SerialNumbers As String() = txtSerialNumber.Text.Split(SpecialCharacter)
+
+        For Each serialNumber In SerialNumbers
+            Dim Found As Boolean = False
+            For i = 0 To WhereWentLines.Length() - 1
+
+                'Console.WriteLine("Serial Number from file: " + (WhereWentLines(i).Split(SpecialCharacter))(0))
+                'Console.WriteLine("Serial Number from textbox: " + txtSerialNumber.Text)
+
+                If serialNumber = RemoveCharacter((WhereWentLines(i).Split(SpecialCharacter))(0), vbCrLf) Then
+                    Found = True
+                    If cmbLocation3.Visible Then
+                        WhereWentLines(i) = serialNumber + SpecialCharacter + cmbLocation1.Text + Separator + cmbLocation2.Text + Separator + cmbLocation3.Text
+                    Else
+                        WhereWentLines(i) = serialNumber + SpecialCharacter + cmbLocation1.Text + Separator + cmbLocation2.Text
+                    End If
+                End If
+
+                If Not Found And i = WhereWentLines.Length() - 1 Then
+                    If cmbLocation3.Visible Then
+                        WhereWentLines(i) += vbCrLf + serialNumber + SpecialCharacter + cmbLocation1.Text + Separator + cmbLocation2.Text + Separator + cmbLocation3.Text
+                    Else
+                        WhereWentLines(i) += vbCrLf + serialNumber + SpecialCharacter + cmbLocation1.Text + Separator + cmbLocation2.Text
+                    End If
+                End If
+            Next
+        Next
+
+        File.WriteAllLines(WhereWent, WhereWentLines)
+        MsgBox("Successfully written to " + WhereWent)
+    End Sub
+
+    Private Sub txtFileLocation_TextChanged(sender As Object, e As EventArgs) Handles txtFileLocation.TextChanged
+        WhereWent = txtFileLocation.Text
+    End Sub
+
+    Private Function RemoveCharacter(ByVal stringToClean, ByVal charRemove)
+        Return stringToClean.Replace(charRemove, "")
+    End Function
+End Class
